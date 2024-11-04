6bbd7ba9b5dc27f2bc77ad4ff6297a197a2d7edc
hafyze <hafizmatqwer@gmail.com>
2024-11-04 14:21:43 +0800

    feat: added register, login, forget password page

--- src/lib/models/user.models.ts
+++ src/lib/models/user.models.ts
@@ -2,17 +2,9 @@ export interface User {
 	id: string;
 	username: string;
 	email: string;
-	passwordHash: string;
+	password: string;
 	firstName: string;
 	lastName: string;
 	createdDt: string;
 	updatedDt: string;
-}
-
-export interface UserLogin {
-	firstName: string;
-	lastName: string;
-	username: string;
-	email: string;
-	password: string;
 }
