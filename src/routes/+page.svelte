f030fc356fb888810c514a830ef524204e99e888
hafyze <hafizmatqwer@gmail.com>
2024-11-27 23:16:11 +0800

    feat: added carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
-	import { Card } from "flowbite-svelte";
-
-
-</script>
-
-
+    import { Carousel, Thumbnails } from "flowbite-svelte";
+    import { images } from "./imageData/+server.js";
+    import Controls from "flowbite-svelte/Controls.svelte";
+    import Indicators from "flowbite-svelte/Indicators.svelte";
+  
+    let index = 0;
+    let forward = true;
+  </script>
+  
+  <div class="max-w-4xl space-y-4 mx-auto">
+    <!-- Carousel Component -->
+    <Carousel 
+      imgClass="object-contain h-full rounded-sm"
+      {images} {forward} let:Indicators let:Controls bind:index>
+      <Controls />
+      <Indicators />
+    </Carousel>
+  
+    <!-- Thumbnails Component -->
+    <Thumbnails {images} {forward} bind:index 
+      imgClass="object-contain w-[80px] h-auto sm:w-[100px] md:w-[120px] lg:w-[150px]" />
+  </div>
+  
---
+++ src/routes/imageData/+server.js
@@ -0,0 +1,32 @@
+export const images = [
+  {
+    alt: 'front',
+    src: '/images/front.jpg',
+    title: 'Front look',
+  },
+  {
+    alt: 'Bed',
+    src: '/images/bed.jpg',
+    title: 'Cozy Bed',
+  },
+  {
+    alt: 'Bed2',
+    src: '/images/bed2.jpg',
+    title: 'Cozy Bed',
+  },
+  {
+    alt: 'Table',
+    src: '/images/table.jpg',
+    title: 'Table',
+  },
+  {
+    alt: 'Kitchen',
+    src: '/images/kitchen.jpg',
+    title: 'Kitchen',
+  },
+  {
+    alt: 'Toilet',
+    src: '/images/toilet.jpg',
+    title: 'Toilet',
+  },
+];
