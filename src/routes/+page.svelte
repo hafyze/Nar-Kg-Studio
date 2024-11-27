da3dc775966af92619292d95394f2f74c5743413
hafyze <hafizmatqwer@gmail.com>
2024-11-27 22:04:09 +0800

    chore: added engines for json

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Card } from "flowbite-svelte";
+
+
+</script>
+
+<div class="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div class="mx-auto">
+        <Card class="text-center p-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg">
+            <p class="text-gray-800 dark:text-white">
+                Welcome to Penang where the food delights you everywhere you go
+            </p>
+        </Card>
+    </div>
+</div>
