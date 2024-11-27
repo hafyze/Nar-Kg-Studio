4f337b65475dbb36fb27361cf7aa218b9834bb25
hafyze <hafizmatqwer@gmail.com>
2024-11-27 22:15:45 +0800

    chore: added pictures

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,4 @@
 
 </script>
 
-<div class="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
-    <div class="mx-auto">
-        <Card class="text-center p-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg">
-            <p class="text-gray-800 dark:text-white">
-                Welcome to Penang where the food delights you everywhere you go
-            </p>
-        </Card>
-    </div>
-</div>
+
