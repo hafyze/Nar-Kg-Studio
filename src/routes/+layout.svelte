f79de76e9e2b4475c0204382206eeb2236ca78b0
hafyze <hafizmatqwer@gmail.com>
2025-01-09 21:20:39 +0800

    chore: change name to NAR Guest Room

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
     <div class="relative px-8">
         <Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b">
             <NavBrand href="/">
-                <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">NAR Kampung Studio</span>
+                <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">NAR Guest Room</span>
               </NavBrand>
 			  <div class="flex md:order-2">
 				<Button size="sm" href="/booking">Book Now</Button>
