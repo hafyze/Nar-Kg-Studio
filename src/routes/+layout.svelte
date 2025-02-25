38be55afd56ee3bec15c42b91b7e4b0b272494ab
hafyze <hafizmatqwer@gmail.com>
2025-02-25 21:38:08 +0800

    feat: added icon for Main Title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import { Button, DarkMode, Navbar, NavBrand, NavHamburger, NavLi, NavUl } from "flowbite-svelte";
   import FeedbackForm from '$lib/components/FeedbackForm.svelte';
+	import { BedOutline } from 'flowbite-svelte-icons';
 
 </script>
 
@@ -10,13 +11,13 @@
     <div class="relative px-8">
         <Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b">
             <NavBrand href="/">
-                <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">NAR Guest Room</span>
+                <span class="flex items-center  self-center whitespace-nowrap text-xl font-semibold dark:text-white"><BedOutline />NAR Guest Room</span>
               </NavBrand>
 			  <div class="flex md:order-2">
 				<Button size="sm" href="/booking">Book Now</Button>
 				<NavHamburger /> 
 			  </div>              <NavUl>
-                <NavLi href="/">Home</NavLi>
+                <NavLi href="/"> Home</NavLi>
                 <NavLi href="/attraction">Attractions</NavLi>
                 <NavLi href="/about">About</NavLi>
                 <NavLi href="/contact">Contact</NavLi>
